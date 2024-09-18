f64e108428c6a4b825d6cd545417b9e6cbbb7489
Faith Kamau <121166087+hiqedme@users.noreply.github.com>
2024-09-18 09:55:50 +0300

    added 7day prediction

--- service/predictions/ml-prediction-service.js
+++ service/predictions/ml-prediction-service.js
@@ -8,11 +8,44 @@ const db = require('../../etl-db');
  */
 const getPredictedScore = async (patientUuid) => {
   return new Promise((resolve, reject) => {
-    const sql = `select ml.* from predictions.ml_weekly_predictions ml 
-        left join amrs.person p on (ml.person_id = p.person_id)
-        where p.uuid = '${patientUuid}' 
-        and p.voided = 0 
-        order by ml.prediction_generated_date desc limit 1;`;
+    const sql = `SELECT 
+    ml.person_id,
+    ml.encounter_id,
+    ml.location_id,
+    ml.rtc_date,
+    CASE
+        WHEN
+            mlfm.prediction_endpoint = 7
+                AND ml.week > '2024-W36'
+        THEN
+            ml.predicted_risk_7day
+        ELSE ml.predicted_risk
+    END AS predicted_risk,
+    CASE
+        WHEN
+            mlfm.prediction_endpoint = 7
+                AND ml.week > '2024-W36'
+        THEN
+            ml.predicted_prob_disengage_7day
+        ELSE ml.predicted_prob_disengage
+    END AS predicted_prob_disengage,
+    ml.observed_rtc_date,
+    ml.prediction_generated_date,
+    ml.model_version,
+    ml.start_date,
+    ml.end_date,
+    ml.week
+FROM
+    predictions.ml_weekly_predictions ml
+        LEFT JOIN
+    amrs.person p ON (ml.person_id = p.person_id)
+        LEFT JOIN
+    predictions.ml_facility_metadata mlfm ON (mlfm.location_id = ml.location_id)
+WHERE
+    p.uuid = '${patientUuid}' 
+        AND p.voided = 0
+ORDER BY ml.prediction_generated_date DESC
+LIMIT 1;`;
 
     const queryParts = {
       sql: sql
